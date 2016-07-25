7a12fa4e6f440161a689fe350700b6c9ed3a3496
David Mazmanian <davidmazmanian@hotmail.com>
2016-07-25 10:31:23 -0400

    First commit

---
+++ src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        public IActionResult Welcome(string name = "Guest", int numTimes = 1)
+        {
+            // return HtmlEncoder.Default.Encode($"Hello {name}, ID: {id}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
